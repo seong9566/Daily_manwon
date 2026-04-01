eb1f92bb04372a03a4eca1991ab42076af067fb5
HyeonSeong <dlgustjd9566@gmail.com>
2026-04-01 15:28:27 +0900

    docs(widget): 홈 위젯 디자인 가이드 및 상태별 UI 이미지 추가, 불필요한 기획 문서 정리

---
+++ ios/DailyHomeWidget/DailyHomeWidgetBundle.swift
@@ -0,0 +1,17 @@
+//
+//  DailyHomeWidgetBundle.swift
+//  DailyHomeWidget
+//
+//  Created by stecdev_mac on 4/1/26.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct DailyHomeWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        DailyHomeWidget()
+        DailyHomeWidgetControl()
+    }
+}
